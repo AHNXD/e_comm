a684ea43b173063c39576a4d964e87306a5393d7
AHNXD <nzamlyalhady@gmail.com>
2024-11-01 12:18:47 +0400

    apply dart fix

--- lib/Future/Home/Widgets/home_screen/carousel_slider_widget.dart
+++ lib/Future/Home/Widgets/home_screen/carousel_slider_widget.dart
@@ -1,6 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:sizer/sizer.dart';
 
 class CarouselSliderWidget extends StatelessWidget {
   const CarouselSliderWidget(
