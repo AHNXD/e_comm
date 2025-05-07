243ad20b79e7fde71d8ef2759eb9b041f40f4474
AHNXD <nzamlyalhady@gmail.com>
2025-05-07 20:28:41 +0400

    last change

--- lib/Future/Home/Cubits/favoriteCubit/favorite_cubit.dart
+++ lib/Future/Home/Cubits/favoriteCubit/favorite_cubit.dart
@@ -10,40 +10,6 @@ part 'favorite_state.dart';
 class FavoriteCubit extends Cubit<FavoriteState> {
   FavoriteCubit() : super(FavoriteInitial());
 
-  // void getProductsFavorite() async {
-  //   FavoriteModel? fvModel;
-  //   emit(GetFavoriteProductsLoadingState());
-  //   try {
-  //     await Network.getData(url: Urls.getProductsFavorite)
-  //         .then((response) async {
-  //       if (response.statusCode == 200 || response.statusCode == 201) {
-  //         fvModel = FavoriteModel.fromJson(response.data);
-  //         List<MainProduct> favProducts = [];
-  //         for (int i = 0; i < fvModel!.data!.length; i++) {
-  //           await Network.getData(
-  //                   url: "${Urls.getProduct}/${fvModel!.data![i].productId}")
-  //               .then((response) {
-  //             MainProduct product = MainProduct.fromJson(response.data["data"]);
-  //             product.isFavorite = true;
-  //             favProducts.add(product);
-  //           });
-  //         }
-  //         emit(GetFavoriteProductsSuccessfulState(fvModel: favProducts));
-  //       }
-  //     });
-  //   } catch (error) {
-  //     if (error is DioException) {
-  //       emit(
-  //         FavoriteProductsErrorState(
-  //           exceptionsHandle(error: error),
-  //         ),
-  //       );
-  //     } else {
-  //       FavoriteProductsErrorState(error.toString());
-  //     }
-  //   }
-  // }
-
   Future<bool> addAndDelFavoriteProducts(int id) async {
     bool isFave = false;
     try {
